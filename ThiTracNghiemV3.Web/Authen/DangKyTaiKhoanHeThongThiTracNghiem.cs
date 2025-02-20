7e305e8862a9f40bc65a9d9a4d7d4bf670ec7ae8
thinh <thinhqn999@gmail.com>
2025-02-20 21:55:16 +0700

    chuẩn bị code làm chức năng mới: đăng ký và làm phần chức năng chính (chức năng thi trắc nghiệm trực tuyến)

---
+++ ThiTracNghiemV3.Web/Authen/DangKyTaiKhoanHeThongThiTracNghiem.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace ThiTracNghiemV3.Web.Authen
+{
+  public class DangKyTaiKhoanHeThongThiTracNghiem : AuthenticationStateProvider
+  {
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+      // hôm sau làm tiếp
+      throw new NotImplementedException();
+    }
+  }
+}
--- ThiTracNghiemV3.Web/Program.cs
+++ ThiTracNghiemV3.Web/Program.cs
@@ -8,6 +8,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
+
 ConfigureRefit(builder.Services);
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
