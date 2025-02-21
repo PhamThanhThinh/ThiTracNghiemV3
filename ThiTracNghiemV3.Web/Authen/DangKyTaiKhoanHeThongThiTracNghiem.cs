6980d4c590283a67c0f59a17b1e8d4ebd36b229c
thinh <thinhqn999@gmail.com>
2025-02-21 21:21:19 +0700

    final buổi 8

--- ThiTracNghiemV3.Web/Authen/DangKyTaiKhoanHeThongThiTracNghiem.cs
+++ ThiTracNghiemV3.Web/Authen/DangKyTaiKhoanHeThongThiTracNghiem.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.JSInterop;
 using System.Security.Claims;
+using System.Security.Principal;
 using ThiTracNghiemV3.Shared;
 
 namespace ThiTracNghiemV3.Web.Authen
@@ -9,14 +10,16 @@ namespace ThiTracNghiemV3.Web.Authen
   public class DangKyTaiKhoanHeThongThiTracNghiem : AuthenticationStateProvider
   {
     private const string AuthenType = "thitracnghiem-authen";
+    private const string UserDataKey = "thitracnghiem-user";
+
 
     private readonly Task<AuthenticationState> _authenStateTask;
     private readonly IJSRuntime _jSRuntime;
 
     public DangKyTaiKhoanHeThongThiTracNghiem(IJSRuntime jSRuntime)
     {
-      var identiy = new ClaimsIdentity();
-      var user = new ClaimsPrincipal(identiy);
+      var identity = new ClaimsIdentity();
+      var user = new ClaimsPrincipal(identity);
       var authenState = new AuthenticationState(user);
 
       _jSRuntime = jSRuntime;
@@ -29,12 +32,31 @@ namespace ThiTracNghiemV3.Web.Authen
     public CheckNguoiDungDangNhap User { get; private set; }
     public bool CheckNullThongTinUser => User?.Id > 0;
 
-    public async Task LoginAsync(CheckNguoiDungDangNhap user)
+    public async Task SetLoginAsync(CheckNguoiDungDangNhap user)
     {
       User = user;
       await _jSRuntime.InvokeVoidAsync("localStorage.setItem", user.ToJson());
 
       var identity = new ClaimsIdentity(user.GetClaims(), AuthenType);
+      var userPrincipal = new ClaimsPrincipal(identity);
+      var authenState = new AuthenticationState(userPrincipal);
+
+      NotifyAuthenticationStateChanged(_authenStateTask);
     }
+
+    public async Task SetLoginAsync()
+    {
+      User = null;
+      await _jSRuntime.InvokeVoidAsync("localStorage.removeItem", UserDataKey);
+    }
+
+    private void SetAuthSateTask()
+    {
+      //if ()
+      //{
+
+      //}
+    }
+
   }
 }
